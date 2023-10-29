24c01d4a467dc0052349dba83c2375645b7bafa6
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-10-29 21:30:07 +0300

    Console.App-Double-ToInt

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_Double_ToInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int s1, s2, reply;
+            //Console.Write("s1: ");
+            //s1=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("s2: ");
+            //s2=Convert.ToInt32(Console.ReadLine());
+            //reply = s1 / s2;
+            //Console.Write(reply);
+            //Console.Read();
+
+
+            //double s1, s2, plus;
+            //Console.Write("s1: ");
+            //s1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("s2: ");
+            //s2 = Convert.ToDouble(Console.ReadLine());
+            //plus = s1 + s2;
+            //Console.Write(plus);
+            //Console.Read();
+        }
+    }
+}
